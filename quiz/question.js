3a8130b2df8c4b1eb42bb77d80ab6f72178215e7
Mamita123 <mamitagurung895@gmail.com>
2022-12-09 13:23:55 +0200

    added quiz

---
+++ quiz/question.js
@@ -0,0 +1,19 @@
+
+
+const quiz = [{
+  question: "How many timezones are there in the world?",
+  option: ["12","16","24","10"],
+  answer:2
+}, {
+  question: "What is the time difference between Pacific time and Eastern time?",
+  option:["Pacific time is 3 hours later","Pacific time is two hours earlier","Pacific time is 3 hours earlier", "Pacific time is 2 hours later"],
+  answer:0
+  },{
+  question: "!Which of these countries has the highest per capita CO2 emissions?",
+  option:[ "USA","China","India","UK"],
+  answer:1
+},{
+
+}
+
+]
