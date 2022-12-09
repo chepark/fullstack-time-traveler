1345792064f668e50a2cd7e7b30ef462777714f3
Mamita123 <mamitagurung895@gmail.com>
2022-12-09 14:15:14 +0200

    added question.js

--- quiz/Javascript1.js
+++ quiz/Javascript1.js
@@ -16,6 +16,7 @@ let availableOptions = [];
 let correctAnswers = 0;
 let attempt = 0;
 
+
 //push the questions into availableQuestions array
 function setAvailableQuestions(){
   const totalQuestion = quiz.length;
--- quiz/question.js
+++ quiz/question.js
@@ -1,6 +1,5 @@
 
-
-const quiz = [{
+let quiz = [{
   question: "How many timezones are there in the world?",
   option: ["12","16","24","10"],
   answer:2
@@ -13,7 +12,34 @@ const quiz = [{
   option:[ "USA","China","India","UK"],
   answer:1
 },{
-
+  question:"what does the abbreviation EST stand for?",
+  option:["East, South Time","Eastern Southern Time","Eastern Standard Time","It doesn't stand anything"],
+  answer:2
+},
+{
+  question:"How many timezones are there in the Finland?",
+  option:["3", "2", "5", "1"],
+  answer:3
+},{
+  question:"The total set of carbon emissions caused by an individual.",
+  option: ["carbon Cycle", "Carbon footprint", "Carbon handprint", "Carbon summary"],
+  answer: 1
+},{
+  question: "What is the most potent greenhouse gas?",
+  option: ["Fluorinated gases"," Carbon dioxide"," Nitrous gas","Methane"],
+  answer: 0
+},{
+  question: "The main human activity adds to an individual's carbon footprint.",
+  option:["usage of alternatives energies.","Respiration and growth","Photosynthesis by plants","Usage of fossils fuel for energy"],
+  answer:1
+},{
+  question:"Which of these choices in transportation produces less greenhouse gas than the others?",
+  option:["Riding a bus","Walking","Taking a train","Riding a car"],
+  answer:1
+},{
+  question: "Which one is not the fossils fuel?",
+  option:["coal", "Natural gas","oil","wood"],
+  answer:3
 }
 
 ]
