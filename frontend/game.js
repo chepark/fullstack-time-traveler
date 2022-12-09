b506a9a331ef9f83733ca84d0176a853998d583a
AnnaLinden <linden.a.anna@gmail.com>
2022-12-09 16:24:25 +0200

    co2 budjet in js

--- frontend/game.js
+++ frontend/game.js
@@ -36,3 +36,31 @@ function showMap() {
 function closeMap() {
   timezonemap.close();
 }
+//CO2 INDICATOR
+
+//function to fetch data from API
+async function getData(url){
+  const response = await fetch(url);
+  if (!response.ok) throw new Error ('Invalid server input!');
+  const data = await response.json();
+  return data
+}
+// function to update game status
+function updateCO2(status) {
+  document.querySelector('#player-name').innerHTML = `Player: ${status.name}`;
+  document.querySelector('#consumed').innerHTML = status.co2.consumed;
+  document.querySelector('#budget').innerHTML = status.co2.budget;
+}
+
+//start te game and calls the other functions
+async function gameSetup(){
+  try{
+    const response = await fetch('');
+    const data = await response.json();
+    console.log(data);
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
